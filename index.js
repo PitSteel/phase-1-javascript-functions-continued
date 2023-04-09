c5e0337056c843035de02a0cbf5c08adc9ab5e34
Ben Campbell <oakcityben@gmail.com>
2023-04-09 11:23:26 -0400

    message

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(mondayActivity = "go to the office") {
+    return `This Monday, I will ${mondayActivity}.`;
+}
+
+function wrapAdjective(star = "*") {
+    return function(value2 = "special") {
+    return `You are ${star}${value2}${star}!`
+    }
+}
+
+wrapAdjective("||")("a dedicated programmer")
